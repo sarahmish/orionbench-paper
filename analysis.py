504a36d93d7c3c13c0afeee1e3fced3a1c14ebd0
sarahmish <sarahalnegheimish@gmail.com>
2023-08-31 02:12:13 -0400

    update figures

--- analysis.py
+++ analysis.py
@@ -21,6 +21,7 @@ from orion.benchmark import BENCHMARK_DATA
 DATASET_FAMILY = {
     "MSL": "NASA",
     "SMAP": "NASA",
+    "UCR": "UCR",
     "YAHOOA1": "YAHOO",
     "YAHOOA2": "YAHOO",
     "YAHOOA3": "YAHOO",
@@ -35,6 +36,7 @@ DATASET_FAMILY = {
 DATASET_ABBREVIATION = {
     "MSL": "MSL",
     "SMAP": "SMAP",
+    "UCR": "UCR",
     "YAHOOA1": "A1",
     "YAHOOA2": "A2",
     "YAHOOA3": "A3",
@@ -54,16 +56,13 @@ GITHUB_URL = 'https://raw.githubusercontent.com/sintel-dev/Orion/master/benchmar
 
 DATA_MAP = {signal: data for data, signals in BENCHMARK_DATA.items() for signal in signals}
 
-_VERSION = ['0.1.3', '0.1.4', '0.1.5', '0.1.6', '0.1.7', '0.2.0', '0.2.1', '0.3.0', '0.3.1', '0.3.2', '0.4.0', '0.4.1', '0.5.0']
-_ORDER = ['aer', 'lstm_dynamic_threshold', 'arima', 'lstm_autoencoder', 'tadgan', 'vae', 'dense_autoencoder', 'ganf', 'azure']
-_LABELS = ['AER', 'LSTM DT', 'ARIMA', 'LSTM AE', 'TadGAN', 'VAE', 'Dense AE', 'GANF', 'Azure AD']
-_COLORS = ["#ED553B", "#F6D55C", "#3CAEA3", "#4287F5", "#20639B", "#173F5F", "#5E3A94", "#4A4A4A", "#8F96A2"]
-_MARKERS = ['o', 's', 'v', 'X', 'p', '^', 'd', 'P', '>']
-
-_PALETTE = sns.color_palette("Spectral")
-_PALETTE.append("#173F5F")
-_PALETTE.append("#4A4A4A")
-_PALETTE.append("#8F96A2")
+_VERSION = ['0.1.3', '0.1.4', '0.1.5', '0.1.6', '0.1.7', '0.2.0', '0.2.1', '0.3.0', '0.3.1', '0.3.2', '0.4.0', '0.4.1', '0.5.0', '0.5.1']
+_ORDER = ['aer', 'lstm_dynamic_threshold', 'arima', 'matrixprofile', 'lstm_autoencoder', 'tadgan', 'vae', 'dense_autoencoder', 'ganf', 'azure', 'anomaly_transformer']
+_LABELS = ['AER', 'LSTM DT', 'ARIMA', 'MP', 'LSTM AE', 'TadGAN', 'VAE', 'Dense AE', 'GANF', 'Azure AD', 'AnomTransformer']
+_COLORS = ['#9B2226', '#AE2012', '#BB3E03', '#EE9B00', '#E9D8A6', '#BFD5B2', '#94D2BD', '#0A9396', '#005F73']
+_NEW_COLORS = ['#9B2226', '#AE2012', '#BB3E03', '#CA6702', '#EE9B00', '#E9D8A6', '#BFD5B2', '#94D2BD', '#0A9396', '#005F73', '#001219']
+_MARKERS = ['o', 's', 'v', 'X', 'p', '^', 'd', 'P', '>', '<', 'H']
+_PALETTE = _NEW_COLORS
 
 
 # ------------------------------------------------------------------------------
@@ -127,7 +126,7 @@ def table_5():
         df = df.groupby(['dataset', 'family', 'pipeline'])[[metric]].agg(["mean", "std"]).droplevel(0, axis=1)
         df['value'] = df["mean"].round(3).astype("str") + "+-" + df["std"].round(2).astype("str")
         df = df[['value']].unstack().T.droplevel(0)
-        df = df[['MSL', 'SMAP', 'A1', 'A2', 'A3', 'A4', 'Art', 'AWS', 'AdEx', 'Traf', 'Tweets']]
+        df = df[['MSL', 'SMAP', 'UCR', 'A1', 'A2', 'A3', 'A4', 'Art', 'AWS', 'AdEx', 'Traf', 'Tweets']]
         df = df.swaplevel(axis=1).loc[_ORDER]
         df.index = _LABELS
         df.name = metric.title()
@@ -152,30 +151,43 @@ def figure_4():
     df = _compute_f1(df)
     df = df.set_index(['dataset', 'pipeline', 'iteration'])[['f1', 'family']].reset_index()
 
-    fig, axes = plt.subplots(1, 4, sharex=True, sharey=True, figsize=(12, 3))
+    fig, axes = plt.subplots(1, 5, sharex=True, sharey=True, figsize=(13, 3))
     fig.subplots_adjust(wspace=0)
+    axes = axes.flatten()
 
     data = df.set_index('pipeline').loc[_ORDER].reset_index()
 
     sns.boxplot(data[data['family'] == 'NASA'], x='pipeline', y='f1', palette=_PALETTE, ax=axes[0])
-    sns.boxplot(data[data['dataset'].isin(['A1', 'A2'])], x='pipeline', y='f1', palette=_PALETTE, ax=axes[1])
-    sns.boxplot(data[data['dataset'].isin(['A3', 'A4'])], x='pipeline', y='f1', palette=_PALETTE, ax=axes[2])
-    sns.boxplot(data[data['family'] == 'NAB'], x='pipeline', y='f1', palette=_PALETTE, ax=axes[3])
+    sns.boxplot(data[data['family'] == 'NAB'], x='pipeline', y='f1', palette=_PALETTE, ax=axes[1])
+    sns.boxplot(data[data['dataset'].isin(['A1', 'A2'])], x='pipeline', y='f1', palette=_PALETTE, ax=axes[2])
+    sns.boxplot(data[data['dataset'].isin(['A3', 'A4'])], x='pipeline', y='f1', palette=_PALETTE, ax=axes[3])
+    sns.boxplot(data[data['family'] == 'UCR'], x='pipeline', y='f1', palette=_PALETTE, ax=axes[4])
 
-    for i in range(4):
+    for i in range(5):
         axes[i].grid(True, linestyle='--')
         axes[i].set_xticklabels(_LABELS, rotation=90)
         axes[i].set_xlabel('')
         axes[i].set_ylabel('')
         
-    axes[0].set_xlim(-1, 9)
+    # axes[2].get_xaxis().set_visible(False)
+    # axes[2].get_yaxis().set_visible(False)
+
+    axes[0].set_xlim(-1, 11)
     axes[0].set_ylabel('F1 Score')
-
+    
     axes[0].set_title("NASA")
-    axes[1].set_title("Yahoo S5 (A1 & A2)")
-    axes[2].set_title("Yahoo S5 (A3 & A4)")
-    axes[3].set_title("NAB")
+    axes[1].set_title("NAB")
+    axes[2].set_title("Yahoo S5 (A1 & A2)")
+    axes[3].set_title("Yahoo S5 (A3 & A4)")
+    axes[4].set_title("UCR")
 
+    # handles = [
+    #      mpatches.Patch(color=_PALETTE[i], label=_LABELS[i])
+    #      for i in range(len(_LABELS))
+    # ]
+
+    # plt.legend(handles=handles, bbox_to_anchor=(0.98, 2.1), frameon=False, ncol=2)
+    
     _savefig(fig, 'figure_4', figdir=OUTPUT_PATH)
 
 
@@ -195,7 +207,7 @@ def figure_5a():
 
     plt.grid(True, linestyle='--')
     plt.yscale('log')
-    plt.ylim([0.1e1, 0.2e3])
+    plt.ylim([0.1e1, 0.2e4])
     plt.ylabel('time in seconds (log)')
     plt.xlabel('')
     plt.title("Pipeline Elapsed Time")
@@ -227,15 +239,18 @@ def figure_5b():
         scores.columns = ['pipeline', version]
         results.append(scores.set_index('pipeline'))
 
+    labels = ['AER', 'LSTM DT', 'ARIMA', 'LSTM AE', 'TadGAN', 'VAE', 'Dense AE', 'GANF', 'Azure AD']
+    order = ['aer', 'lstm_dynamic_threshold', 'arima', 'lstm_autoencoder', 'tadgan', 'vae', 'dense_autoencoder', 'ganf', 'azure']
+
     df = pd.concat(results, axis=1)
     df = df.reindex(sorted(df.columns), axis=1)
-    df = df.loc[_ORDER]
+    df = df.loc[order]
 
     fig = plt.figure(figsize=(5, 3))
     ax = plt.gca()
 
     for i, pipeline in enumerate(df.T.columns):
-        ax.plot(df.T[pipeline], marker=_MARKERS[i], markersize=7, color=_PALETTE[i], label=_LABELS[i])
+        ax.plot(df.T[pipeline], marker=_MARKERS[i], markersize=7, color=_COLORS[i], label=labels[i])
 
     plt.grid(True, linestyle='--')
     plt.legend(bbox_to_anchor=(1.01, 0.93), edgecolor='black')
@@ -257,9 +272,10 @@ def figure_9():
     df = pd.read_csv('benchmark.csv')
     df = _compute_f1(df)
 
-    fig, axes = plt.subplots(3, 5, sharex=True, sharey=False, figsize=(14, 9))
+    fig, axes = plt.subplots(3, 5, sharex=True, sharey=True, figsize=(14, 9))
+    fig.subplots_adjust(wspace=0)
     axes = axes.flatten()
-    skip = [2, 3, 4, 9]
+    skip = [3, 4, 9]
     for i in skip:
         fig.delaxes(axes[i])
 
@@ -278,7 +294,7 @@ def figure_9():
         axes[i].set_title(d)
         i += 1
             
-    axes[0].set_xlim(-1, 9)
+    axes[0].set_xlim(-1, 11)
     axes[0].set_ylabel('F1 Score')
     axes[5].set_ylabel('F1 Score')
     axes[10].set_ylabel('F1 Score')
@@ -290,7 +306,7 @@ def figure_9():
          for i in range(len(_LABELS))
     ]
 
-    plt.legend(handles=handles, bbox_to_anchor=(0.81, 2.2), edgecolor='black')
+    plt.legend(handles=handles, bbox_to_anchor=(0.99, 2.4), edgecolor='black')
     _savefig(fig, 'figure_9', figdir=OUTPUT_PATH)
 
 
@@ -301,15 +317,16 @@ def figure_10():
     df = df.set_index('pipeline').loc[_ORDER].reset_index()
 
     fig, axes = plt.subplots(3, 5, sharex=True, sharey=True, figsize=(14, 9))
+    fig.subplots_adjust(wspace=0)
     axes = axes.flatten()
-    skip = [2, 3, 4, 9]
+    skip = [3, 4, 9]
     for i in skip:
         fig.delaxes(axes[i])
 
     data = df.set_index('pipeline').loc[_ORDER].reset_index()
 
     i = 0
-    for d in DATASET_ABBREVIATION.values():
+    for d in DATASET_ABBREVIATION.values():     
         while i in skip:
             i += 1
             
@@ -322,7 +339,7 @@ def figure_10():
         axes[i].set_yscale('log')
         i += 1
         
-    axes[0].set_xlim(-1, 9)
+    axes[0].set_xlim(-1, 11)
     axes[0].set_ylabel('Time (s)')
     axes[5].set_ylabel('Time (s)')
     axes[10].set_ylabel('Time (s)')
@@ -334,7 +351,7 @@ def figure_10():
          for i in range(len(_LABELS))
     ]
 
-    plt.legend(handles=handles, bbox_to_anchor=(0.81, 2.2), edgecolor='black');
+    plt.legend(handles=handles, bbox_to_anchor=(0.99, 2.4), edgecolor='black');
     _savefig(fig, 'figure_10', figdir=OUTPUT_PATH)
 
 
@@ -360,15 +377,18 @@ def figure_11():
         time.columns = ['pipeline', version]
         results.append(time.set_index('pipeline'))
 
+    labels = ['AER', 'LSTM DT', 'ARIMA', 'LSTM AE', 'TadGAN', 'VAE', 'Dense AE', 'GANF', 'Azure AD']
+    order = ['aer', 'lstm_dynamic_threshold', 'arima', 'lstm_autoencoder', 'tadgan', 'vae', 'dense_autoencoder', 'ganf', 'azure']
+    
     df = pd.concat(results, axis=1)
     df = df.reindex(sorted(df.columns), axis=1)
-    df = df.loc[_ORDER]
+    df = df.loc[order]
 
     fig = plt.figure(figsize=(5, 3))
     ax = plt.gca()
 
     for i, pipeline in enumerate(df.T.columns):
-        ax.plot(df.T[pipeline], marker=_MARKERS[i], markersize=7, color=_PALETTE[i], label=_LABELS[i])
+        ax.plot(df.T[pipeline], marker=_MARKERS[i], markersize=7, color=_COLORS[i], label=labels[i])
 
     ax.set_yscale('log')
 
